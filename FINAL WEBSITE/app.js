55335203d77e3e22a4792a9a729e5cef123dde03
Cyrus RJ Sagun <99708681+hunterxhunt1@users.noreply.github.com>
2022-02-22 21:18:45 -0800

    Add files via upload

---
+++ FINAL WEBSITE/app.js
@@ -0,0 +1,8 @@
+
+const menu = document.querySelector("#menu")
+const menuLinks = documents.querySelector(".bar__menu")
+
+menu.addEventListener('click', function(){
+	menu.classList.toggle("is-active")
+	menuLinks.classList.toggle("active")
+})
