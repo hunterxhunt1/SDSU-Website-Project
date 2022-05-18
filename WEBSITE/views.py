1a3c640d39499598e0aa993dd9daa1a8fbe6bd91
Cyrus RJ Sagun <99708681+hunterxhunt1@users.noreply.github.com>
2022-05-17 19:19:59 -0700

    Delete views.py

--- WEBSITE/views.py
+++
@@ -1,8 +0,0 @@
-# THIS IS GOING TO STORE THE ROUTES THAT THE USER WILL GO TO #
-from flask import Blueprint, render_template
-
-views = Blueprint('views', __name__)
-
-@views.route('/')
-def home():
-	return render_template("First Page.html")
